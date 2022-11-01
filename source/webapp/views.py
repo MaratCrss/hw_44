affee51cf133c11b6ecb45c1d7d66aa88dd0092e
Marat <marat.mashanlo@gmail.com>
2022-11-01 18:42:06 +0600

    third commit

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
 def play_game(request):
-    return render(request, index.html)
+    combination = [3, 5, 7, 8]
+    if request.method == "GET":
+        context = {
+            "result": None,
+            "message": "message",
+        }
+        return render(request, 'index.html')
